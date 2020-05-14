d1760d85af9b58269e539e76196a1bf6a472713e
Romandsom <59917503+Romandsom@users.noreply.github.com>
2020-05-14 18:08:51 +0300

    Update cargo_car.rb

--- Lesson_5/cargo_car.rb
+++ Lesson_5/cargo_car.rb
@@ -2,11 +2,9 @@ class CargoCar
   extend InstanceCounter::ClassMethods
   include BrandNaming
   attr_reader :type_of_car, :number
-  @@cargocar_roster = []
 
   def initialize(number)
     @number = number
     @type_of_car = 'cargo'
-    @@cargocar_roster << self
   end
 end
