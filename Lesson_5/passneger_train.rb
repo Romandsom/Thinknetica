a3db5d0860ed3265c548e202e4ba2306a26d664a
Romandsom <59917503+Romandsom@users.noreply.github.com>
2020-05-14 18:13:28 +0300

    Update passneger_train.rb

--- Lesson_5/passneger_train.rb
+++ Lesson_5/passneger_train.rb
@@ -1,10 +1,4 @@
 class PassengerTrain < Train
-  @@passengertrain_roster = []
-
-  def initialize(speed = 0,number)
-    @@passengertrain_roster << self
-    super
-  end
 
   def attach_car(car)
     if car.class == PassengerCar
